944ee9496f369c798c3066c049ebe8ef190c0909
Kate Arvay <kate@arvay.cz>
2021-10-19 22:22:46 +0200

    add play and pause class methods

--- main.js
+++ main.js
@@ -1,17 +1,50 @@
 class MyPlayer {
+    playerElement = "";
     constructor(divId, width, height) {
         this.divId = divId;
         this.width = width;
         this.height = height;
-    }
-    load(url) {
-        this.divId.innerHTML = `<video src=${url} width=${this.width} height=${this.height}/>`
     };
-   
-};
+    load(url) {
+        this.divId.innerHTML = `<video id="playerElement" src=${url} width=${this.width} height=${this.height} controls  />`;
+        this.playerElement = document.getElementById("playerElement");
+    };
+    play() {
+        if(this.playerElement) {
+            return this.playerElement.play();
+    }
+    else {
+        return console.log("no video to play");
+    }
 
-window.onload = function() {
+};
+    pause() {
+        if(this.playerElement) {
+        return this.playerElement.pause();          
+    }
+    else {
+        return console.log("no video to pause");
+    }
+    };
+}
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
     const player = new MyPlayer(document.getElementById("player"), 600, 400);
     player.load("https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4");
-};
+    document.getElementById('play-button').addEventListener('click', (event) => {
+        player.play();
+    });
+    document.getElementById('pause-button').addEventListener('click', (event) => {
+        player.pause();
+    })
+});
+
+
+
+
+
+
+
+
 
