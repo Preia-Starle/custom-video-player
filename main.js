d0bf5d35f10dc461ac2ce917de6abdd12aae668a
Kate Arvay <kate@arvay.cz>
2021-10-20 13:59:03 +0200

    add getHeight and getWidth class methods

--- main.js
+++ main.js
@@ -10,25 +10,33 @@ class MyPlayer {
         this.playerElement = document.getElementById("playerElement");
     };
     play() {
-        if(this.playerElement) {
+        if (this.playerElement) {
             return this.playerElement.play();
-    }
-    else {
-        return console.log("no video to play");
-    }
+        }
+        else {
+            return console.log("no video to play");
+        }
 
-};
+    };
     pause() {
-        if(this.playerElement) {
-        return this.playerElement.pause();          
-    }
-    else {
-        return console.log("no video to pause");
-    }
+        if (this.playerElement) {
+            return this.playerElement.pause();
+        }
+        else {
+            return console.log("no video to pause");
+        }
     };
     resize(width, height) {
         this.playerElement.setAttribute("width", width);
         this.playerElement.setAttribute("height", height);
+    };
+    getHeight() {
+        console.log(this.height);
+        return this.height;
+    };
+    getWidth() {
+        console.log(this.width);
+        return this.width;
     };
 }
 
@@ -36,17 +44,26 @@ class MyPlayer {
 window.addEventListener('DOMContentLoaded', (event) => {
     const player = new MyPlayer(document.getElementById("player"), 600, 400);
     player.load("https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4");
-    document.getElementById('play-button').addEventListener('click', (event) => {
+    document.getElementById("play-button").addEventListener("click", (event) => {
         player.play();
     });
-    document.getElementById('pause-button').addEventListener('click', (event) => {
+    document.getElementById("pause-button").addEventListener("click", (event) => {
         player.pause();
-    })
+    });
     document.getElementById("resize-button").addEventListener("click", (event) => {
         let newWidth = document.getElementById("width-input").value;
         let newHeight = document.getElementById("height-input").value;
         player.resize(newWidth, newHeight);
-});
+    });
+    document.getElementById("get-height").addEventListener("click", (event) => {
+        let heightOutput = document.getElementById("height-output");
+        heightOutput.innerHTML = `<td>${player.getHeight()}</td>`
+    })
+    document.getElementById("get-width").addEventListener("click", (event) => {
+        let widthOutput = document.getElementById("width-output");
+        widthOutput.innerHTML = `<td>${player.getWidth()}</td>`
+    })
+
 });
 
 
